d3f8455f03f6824f12dd8be2aec45ee62bb48235
POHNAH <wolf.programmer@gmail.com>
2021-01-05 18:33:47 +0300

    проверка работы с гитхабом

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ dp = Dispatcher(bot, loop=loop)
 if __name__ == "__main__":
     from hendlers import dp, send_to_admin
     executor.start_polling(dp, on_startup=send_to_admin)
+
+# Проверка работы с гитхабм
